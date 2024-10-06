7fd1dc07cba8d3bcf4b99df039d9c9e3afe605d1
Choiseungpyo <86085827+Choiseungpyo@users.noreply.github.com>
2024-10-06 21:32:16 +0900

    백준 문제풀이
    단계 : 1차원배열
    문제번호 : 5597

---
+++ BaekJoon/1차원배열/7.py
@@ -0,0 +1,34 @@
+# 출석부 설정
+def setAttendanceBook(maxStudentNum, submittedStudentNum):
+    attendanceBook = []
+    unsubmittedStudentBook = []
+    minNum = maxStudentNum+1
+
+    for i in range(submittedStudentNum):
+        x = int(input())
+        if x < 1 or x > maxStudentNum :
+            print("값의 범위가 올바르지 않습니다.")
+            return
+        if attendanceBook.count(x) == 0:
+            attendanceBook.append(x)
+        else:
+            print("이미 입력한 값입니다.") 
+            return
+    
+    attendanceBook.sort()
+
+    for i in range(1, maxStudentNum+1):
+        if attendanceBook.count(i) == 0:
+            unsubmittedStudentBook.append(i)
+            if i < minNum: 
+                minNum  = i
+    
+    print(minNum)
+    for i in unsubmittedStudentBook:
+        if minNum == i:
+            continue
+        print(i)
+    
+
+# Main
+setAttendanceBook(5, 3)
