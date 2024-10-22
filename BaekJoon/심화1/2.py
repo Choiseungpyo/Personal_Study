c8f7f56e0e5ddad2075fed8ca5e474a100b0c65f
Choiseungpyo <86085827+Choiseungpyo@users.noreply.github.com>
2024-10-22 15:15:08 +0900

    백준 문제풀이 1/0/1
    총 문제 : 1
    성공한 문제 : 1 - 심화 1 : 10988

--- BaekJoon/심화1/2.py
+++ BaekJoon/심화1/2.py
@@ -1,5 +1,4 @@
 chessSet = [1,1,2,2,2,8]
 chessSetCnt = list(map(int, input().split(' ')))
-print(chessSetCnt)
 for i in range(len(chessSet)):
   print(chessSet[i] - chessSetCnt[i], end = ' ')
---
+++ BaekJoon/심화1/4.py
@@ -0,0 +1,2 @@
+storage = [i for i in input()]
+print(1) if storage == storage[::-1] else print(0)
