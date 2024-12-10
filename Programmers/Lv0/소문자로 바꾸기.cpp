4ced643886bfc1064b79fa5009a9fde9c5658907
Choiseungpyo <86085827+Choiseungpyo@users.noreply.github.com>
2024-12-10 20:31:35 +0900

    Programmers 1/0/1
    총 문제 : 1
    성공한 문제 : 1 - Lv0 : 소문자로 바꾸기

---
+++ Programmers/Lv0/소문자로 바꾸기.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* myString) {
+    int size = strlen(myString) + 1;
+    char* answer = (char*)malloc(size);
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        if (myString[i] < 'a' || myString[i] > 'z')
+            answer[i] = 'a' + myString[i] - 'A';
+        else
+            answer[i] = myString[i];
+    }
+    answer[size - 1] = 0;
+    return answer;
+}
