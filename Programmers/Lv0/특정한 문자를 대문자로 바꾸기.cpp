06f6f3ebbbcc7b7748187856db704d5d0cdc8bf8
Choiseungpyo <86085827+Choiseungpyo@users.noreply.github.com>
2024-12-12 19:52:25 +0900

    Programmers 1/0/1
    총 문제 : 1
    성공한 문제 : 1 - Lv0 : 특정한 문자를 대문자로 바꾸기

---
+++ Programmers/Lv0/특정한 문자를 대문자로 바꾸기.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* solution(const char* my_string, const char* alp) {
+    int size = strlen(my_string);
+    char* answer = (char*)malloc(size + 1);
+
+    for (int i = 0; i < size; i++)
+    {
+        if (my_string[i] == alp[0])
+            answer[i] = 'A' + my_string[i] - 'a';
+        else
+            answer[i] = my_string[i];
+    }
+    answer[size] = 0;
+
+    return answer;
+}
