4427976b6d5d901e329a75a26835a1c6b60aa340
Choiseungpyo <86085827+Choiseungpyo@users.noreply.github.com>
2025-01-07 22:00:36 +0900

    Programmers 1/0/1
    총 문제 : 1
    성공한 문제 : 1 - Lv0 : 짝수는 싫어요

--- Programmers/Programmers/main.cpp
+++ Programmers/Programmers/main.cpp
@@ -2,14 +2,19 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int solution(int num1, int num2) {
-    return (float)num1 / num2 * 1000;
-}
+int* solution(int n) {
+    int size = n % 2 == 0 ? n/2 : n/2 + 1;
+    int* answer = (int*)malloc(sizeof(int) * size);
 
+    for (int i = 0; i < size; i++)
+        answer[i] = 2 * i + 1;
+
+    return answer;
+}
 int main()
 {
     const char* str_list[] = { "problemsolving", "practiceguitar", "swim", "studygraph" };
     bool fubusged[] = { true, false, true, false };
-    int a = solution(3, 2);
+    int* a = solution(10);
     return 0;
 }
