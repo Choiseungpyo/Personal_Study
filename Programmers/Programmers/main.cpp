885e3a5b460c0f82c053234537e47bb1d891a5ce
Choiseungpyo <86085827+Choiseungpyo@users.noreply.github.com>
2025-01-13 09:02:56 +0900

    Programmers 1/0/1
    총 문제 : 1
    성공한 문제 : 1 - Lv0 : 문자열 뒤집기

--- Programmers/Programmers/main.cpp
+++ Programmers/Programmers/main.cpp
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
-int solution(int n, int k) {
-    return 12000 * n + 2000 * (k-n/10);
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* my_string) {
+    int size = strlen(my_string);
+    char* answer = (char*)malloc(size+1);
+
+    for (int i = 0; i < size; i++)
+        answer[i] = my_string[size - i - 1];
+    answer[size] = 0;
+
+    return answer;
 }
+
 int main()
 {
     const char* str_list[] = { "problemsolving", "practiceguitar", "swim", "studygraph" };
     bool fubusged[] = { true, false, true, false };
-    int a = solution(64, 6);
+    char* a = solution("jaron");
     return 0;
 }
