978f252100653e4159583a10a2533670f7a2d2b3
Choiseungpyo <86085827+Choiseungpyo@users.noreply.github.com>
2025-01-14 22:49:20 +0900

    Programmers 1/1/2
    총 문제 : 2
    성공한 문제 : 1 - Lv0 : 배열 뒤집기
    실패한 문제 : 1 - Lv1 : 달리기 경주 (시간 초과)

--- Programmers/Lv1/달리기 경주.cpp
+++ Programmers/Lv1/달리기 경주.cpp
@@ -8,11 +8,11 @@ char** solution(const char* players[], size_t players_len, const char* callings[
 
     for (int i = 0; i < players_len; i++)
     {
-        answer[i] = (char*)malloc(strlen(players[i])+1);
+        answer[i] = (char*)malloc(strlen(players[i]) + 1);
         strcpy(answer[i], players[i]);
     }
 
-   
+
     for (int i = 0; i < callings_len; i++)
     {
         for (int a = 0; a < players_len; a++)
@@ -20,24 +20,15 @@ char** solution(const char* players[], size_t players_len, const char* callings[
             if (strcmp(callings[i], answer[a]))
                 continue;
 
-            char* tmp = (char*)malloc(strlen(answer[a - 1]));
+            char* tmp = (char*)malloc(strlen(answer[a - 1]) + 1);
             strcpy(tmp, answer[a - 1]);
 
-            strcpy(answer[a-1], answer[a]);
+            strcpy(answer[a - 1], answer[a]);
             strcpy(answer[a], tmp);
             break;
         }
     }
-    
+
 
     return answer;
-}
-
-int main()
-{
-    const char* players[] = { "mumu", "soe", "poe", "kai", "mine" };
-    const char* callings[] = { "kai", "kai", "mine", "mine" };
-
-    char** a = solution(players, 5, callings, 4);
-    return 0;
 }
