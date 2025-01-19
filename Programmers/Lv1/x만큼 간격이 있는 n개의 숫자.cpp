118f617a7721f8a4d3c816aca7f07a1dcced4464
Choiseungpyo <86085827+Choiseungpyo@users.noreply.github.com>
2025-01-19 21:46:33 +0900

    Programmers 1/0/1
    총 문제 : 1
    성공한 문제 : 1 - Lv1 : x만큼 간격이 있는 n개의 숫자

--- Programmers/Lv1/x만큼 간격이 있는 n개의 숫자.cpp
+++ Programmers/Lv1/x만큼 간격이 있는 n개의 숫자.cpp
@@ -5,13 +5,7 @@ using namespace std;
 
 vector<long long> solution(int x, int n) {
     vector<long long> answer;
-    for (int i = 0; i < n; i ++)
+    for (int i = 0; i < n; i++)
         answer.push_back(x + x * i);
     return answer;
-}
-
-int main()
-{
-    auto s = solution(-4, 2);
-    return 0;
 }
