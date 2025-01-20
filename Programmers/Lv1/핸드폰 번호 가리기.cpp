bec679f40e8cb4caf832953bbcd4c992a3aeac2d
Choiseungpyo <86085827+Choiseungpyo@users.noreply.github.com>
2025-01-20 23:44:20 +0900

    Programmers 1/0/1
    총 문제 : 1
    성공한 문제 : 1 - Lv1 : 핸드폰 번호 가리기

--- Programmers/Lv1/핸드폰 번호 가리기.cpp
+++ Programmers/Lv1/핸드폰 번호 가리기.cpp
@@ -3,10 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 char* solution(const char* phone_number) {
     int size = strlen(phone_number);
-    char* answer = (char*)malloc(size+1);
+    char* answer = (char*)malloc(size + 1);
     for (int i = 0; i < size; i++)
     {
         if (i < size - 4)
@@ -17,10 +16,4 @@ char* solution(const char* phone_number) {
     answer[size] = 0;
 
     return answer;
-}
-
-int main()
-{
-    auto s = solution("027778888");
-    return 0;
 }
