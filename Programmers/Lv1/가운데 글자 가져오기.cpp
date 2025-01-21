f023e722f1c6f093e9690eb11d780c3b313fcdb6
Choiseungpyo <86085827+Choiseungpyo@users.noreply.github.com>
2025-01-21 21:06:18 +0900

    Programmers 1/0/1
    총 문제 : 1
    성공한 문제 : 1 - Lv1 : 가운데 글자 가져오기

--- Programmers/Lv1/가운데 글자 가져오기.cpp
+++ Programmers/Lv1/가운데 글자 가져오기.cpp
@@ -6,17 +6,11 @@
 char* solution(const char* s) {
     int sSize = strlen(s);
     int answerSize = sSize % 2 == 0 ? 2 : 1;
-    char* answer = (char*)malloc(answerSize +1);
+    char* answer = (char*)malloc(answerSize + 1);
 
     for (int i = 0; i < answerSize; i++)
-        answer[i] = s[answerSize == 1 ? sSize / 2 : sSize / 2 -1 + i];
+        answer[i] = s[answerSize == 1 ? sSize / 2 : sSize / 2 - 1 + i];
     answer[answerSize] = 0;
 
     return answer;
-}
-
-int main()
-{
-    auto s = solution("qwer");
-    return 0;
 }
