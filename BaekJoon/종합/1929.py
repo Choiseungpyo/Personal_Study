5d43db078dc89c1ac92a9649dc1c3184d4a37709
Choiseungpyo <86085827+Choiseungpyo@users.noreply.github.com>
2025-01-25 17:45:48 +0900

    Solved 문제풀이 1/0/1
    - 전에 틀렸던 문제 해결 -
    총 문제 : 1
    성공한 문제 : 1 - Class1 : 1929(소수 구하기)

--- BaekJoon/종합/1929.py
+++ BaekJoon/종합/1929.py
@@ -1,8 +1,10 @@
 import sys
 m, n = map(int, sys.stdin.readline().split())
-for num in range(m,n+1):
+for num in range(m, n+1):
     flag = True
-    for i in range(2,num):
+    if num == 1:
+        continue
+    for i in range(2, int(num**0.5)+1):
         if num % i == 0:
             flag = False
             break
