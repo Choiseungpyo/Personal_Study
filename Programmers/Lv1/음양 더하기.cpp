0d79eac0eba19870e58d85fbb1296471a4171eb5
Choiseungpyo <86085827+Choiseungpyo@users.noreply.github.com>
2025-02-02 19:42:48 +0900

    Programmers 1/0/1
    총 문제 : 1
    성공한 문제 : 1 - Lv1 : 음양 더하기

--- Programmers/Lv1/음양 더하기.cpp
+++ Programmers/Lv1/음양 더하기.cpp
@@ -2,8 +2,6 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-// absolutes_len은 배열 absolutes의 길이입니다.
-// signs_len은 배열 signs의 길이입니다.
 int solution(int absolutes[], size_t absolutes_len, bool signs[], size_t signs_len) {
     int answer = 0;
 
@@ -16,12 +14,4 @@ int solution(int absolutes[], size_t absolutes_len, bool signs[], size_t signs_l
     }
 
     return answer;
-}
-
-int main()
-{
-    int absolutes[] = { 1,2,3 };
-    bool signs[] = { false,false,true };
-    auto result = solution(absolutes, 3, signs, 3);
-    return 0;
 }
