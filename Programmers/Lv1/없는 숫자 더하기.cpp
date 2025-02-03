b934c18bfcf9c4a1e8db3e8ca4be1aceb5701a1f
Choiseungpyo <86085827+Choiseungpyo@users.noreply.github.com>
2025-02-03 23:40:36 +0900

    Programmers 1/0/1
    총 문제 : 1
    성공한 문제 : 1 - Lv1 : 없는 숫자 더하기

--- Programmers/Lv1/없는 숫자 더하기.cpp
+++ Programmers/Lv1/없는 숫자 더하기.cpp
@@ -1,20 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#define NUM 9
 
 int solution(int numbers[], size_t numbers_len) {
-    int answer = 45;
+    int answer = (1 + NUM) * NUM / 2;
 
     for (int i = 0; i < numbers_len; i++)
         answer -= numbers[i];
 
     return answer;
-}
-
-int main()
-{
-    int absolutes[] = { 5,8,4,0,6,7,9 };
-    bool signs[] = { false,false,true };
-    auto result = solution(absolutes, 7);
-    return 0;
 }
