71a7a09f71d278b79df34d6af8224e89c0ddfa5c
Choiseungpyo <86085827+Choiseungpyo@users.noreply.github.com>
2025-06-06 19:06:48 +0900

    Programmers 1/0/1
    총 문제 : 1
    성공한 문제 : 1 - 게임 맵 최단거리

--- Programmers/알고리즘/게임맵최단거리.cpp
+++ Programmers/알고리즘/게임맵최단거리.cpp
@@ -11,10 +11,10 @@ int solution(vector<vector<int>> maps) {
     queue<pair<int, int>> q;
 
     vector<pair<int, int>> directions = {
-        {-1, 0}, // 위
-        {1, 0},  // 아래
-        {0, -1}, // 왼쪽
-        {0, 1}   // 오른쪽
+        {-1, 0},
+        {1, 0},
+        {0, -1},
+        {0, 1}
     };
 
     q.push({ 0, 0 });
@@ -32,21 +32,18 @@ int solution(vector<vector<int>> maps) {
             return dist[row][col];
 
         for (int i = 0; i < directions.size(); i++) {
-            int currRow = row + directions[i].first;
-            int currCol = col + directions[i].second;
+            int newRow = row + directions[i].first;
+            int newCol = col + directions[i].second;
 
-            if (currRow >= 0 && currRow < maxRow && currCol >= 0 && currCol < maxCol) {
-                if (!visited[currRow][currCol] && maps[currRow][currCol] == 1) {
-                    visited[currRow][currCol] = true;
-                    dist[currRow][currCol] = dist[row][col] + 1;
-                    q.push({ currRow, currCol });
+            if (newRow >= 0 && newRow < maxRow && newCol >= 0 && newCol < maxCol) {
+                if (!visited[newRow][newCol] && maps[newRow][newCol] == 1) {
+                    visited[newRow][newCol] = true;
+                    dist[newRow][newCol] = dist[row][col] + 1;
+                    q.push({ newRow, newCol });
                 }
             }
         }
     }
 
     return -1;
-}
-int main() {
-    return 0;
 }
