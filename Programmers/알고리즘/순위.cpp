617c2bba4cd43610547fb9a2366541567435f5af
Choiseungpyo <86085827+Choiseungpyo@users.noreply.github.com>
2025-06-08 15:31:23 +0900

    Programmers 1/0/1
    총 문제 : 1
    성공한 문제 : 1 - 순위

--- Programmers/알고리즘/순위.cpp
+++ Programmers/알고리즘/순위.cpp
@@ -7,19 +7,19 @@ int solution(int n, vector<vector<int>> results) {
     int answer = 0;
     vector<vector<bool>> graph(n, vector<bool>(n, false));
 
-    for (auto& result : results) 
+    for (auto& result : results)
     {
         int winner = result[0] - 1;
         int loser = result[1] - 1;
         graph[winner][loser] = true;
     }
 
-    for (int mid = 0; mid < n; mid++) 
-    {           
-        for (int start = 0; start < n; start++) 
-        { 
+    for (int mid = 0; mid < n; mid++)
+    {
+        for (int start = 0; start < n; start++)
+        {
             for (int end = 0; end < n; end++)
-            {   
+            {
                 if (graph[start][mid] && graph[mid][end])
                     graph[start][end] = true;
             }
@@ -30,9 +30,9 @@ int solution(int n, vector<vector<int>> results) {
     {
         int count = 0;
 
-        for (int other = 0; other < n; other++) 
+        for (int other = 0; other < n; other++)
         {
-            if (player == other) 
+            if (player == other)
                 continue;
 
             if (graph[player][other] || graph[other][player])
