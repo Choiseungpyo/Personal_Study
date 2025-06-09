d5c0d6885aa715e5eaa89a6740d9c077cd339d14
Choiseungpyo <86085827+Choiseungpyo@users.noreply.github.com>
2025-06-09 22:43:29 +0900

    Programmers 1/0/1
    총 문제 : 1
    성공한 문제 : 1 - 가장 먼 노드

--- Programmers/알고리즘/가장 먼 노드.cpp
+++ Programmers/알고리즘/가장 먼 노드.cpp
@@ -5,27 +5,27 @@ using namespace std;
 
 int solution(int n, vector<vector<int>> edge) {
     int answer = 0;
-    vector<vector<int>> graph(n);   
+    vector<vector<int>> graph(n);
     vector<int> distance(n, -1);
     queue<int> q;
     int max_dist = -1;
 
-    for (const auto& e : edge) 
+    for (const auto& e : edge)
     {
         int a = e[0] - 1;
         int b = e[1] - 1;
         graph[a].push_back(b);
         graph[b].push_back(a);
     }
-  
-    q.push(0);            
+
+    q.push(0);
     distance[0] = 0;
 
     while (!q.empty()) {
         int node = q.front();
         q.pop();
 
-        for (int neighbor : graph[node]) 
+        for (int neighbor : graph[node])
         {
             if (distance[neighbor] != -1)
                 continue;
@@ -34,7 +34,7 @@ int solution(int n, vector<vector<int>> edge) {
             q.push(neighbor);
         }
     }
-    
+
     for (int d : distance)
     {
         if (d > max_dist)
@@ -42,7 +42,7 @@ int solution(int n, vector<vector<int>> edge) {
             max_dist = d;
             answer = 1;
         }
-        else if(d == max_dist)
+        else if (d == max_dist)
             answer++;
     }
 
