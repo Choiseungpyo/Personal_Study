4f3fb8ff107c9956f74eed606e0a4d9615d5b960
Choiseungpyo <86085827+Choiseungpyo@users.noreply.github.com>
2025-07-08 23:25:08 +0900

    Solved 1/0/1 - 다빈치코딩 알고리즘 : 25377(빵)
    총 문제 : 1
    성공한 문제 : 1 - 다빈치코딩 알고리즘 - 구현 - 25377(빵)

---
+++ Solved/다빈치코딩 알고리즘/구현/25377(빵).py
@@ -0,0 +1,15 @@
+N = int(input())
+
+MAX = 10000
+result = MAX
+for _ in range(N):
+    arrival_time, open_time = map(int, input().split())
+    if open_time < arrival_time:
+        continue
+
+    if open_time < result:
+        result = open_time
+
+if result == MAX:
+    result = -1
+print(result)
