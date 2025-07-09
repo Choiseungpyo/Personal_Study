72fa9d05082a5052a57d3d58680c7b5b3859d4fe
Choiseungpyo <86085827+Choiseungpyo@users.noreply.github.com>
2025-07-09 22:58:44 +0900

    Programmers 1/0/1 - Lv1 : 수박수박수박수박수박수?
    총 문제 : 1
    성공한 문제 : 1 - Lv1 : 수박수박수박수박수박수?

---
+++ Programmers/Lv1/수박수박수박수박수박수.cpp
@@ -0,0 +1,10 @@
+#include <string>
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    for (int i = 0; i < n; i++) 
+        answer += (i % 2 == 0 ? "¼ö" : "¹Ú");
+    
+    return answer;
+}
