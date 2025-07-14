2d218d660a130ae6a840180dc6e4470eb493d431
Choiseungpyo <86085827+Choiseungpyo@users.noreply.github.com>
2025-07-14 20:18:49 +0900

    Solved 1/0/1 - 다빈치코딩 알고리즘 : 19941(햄버거 분배)
    총 문제 : 1
    성공한 문제 : 다빈치코딩 알고리즘 - 중급알고리즘 - 그리디 알고리즘 - 19941(햄버거 분배)

---
+++ Solved/다빈치코딩 알고리즘/중급 알고리즘/그리디 알고리즘/19941(햄버거 분배).py
@@ -0,0 +1,14 @@
+N, K = map(int, input().split())
+arr = list(input())
+cnt = 0
+for i in range(N):
+    if arr[i] != 'P':
+        continue
+    
+    for j in range(i-K, i+K+1):
+        if 0 <= j < N:
+            if arr[j] == 'H':
+                arr[j] = '-'
+                cnt += 1
+                break
+print(cnt)
