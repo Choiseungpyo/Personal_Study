d201a6679431450113e357d6173ed1a0126196f8
Choiseungpyo <86085827+Choiseungpyo@users.noreply.github.com>
2026-01-29 23:05:44 +0900

    Programmers 1문제
    총 문제 : 1
    <Lv0 : 1문제>
     - 코딩 기초 트레이닝  - 배열의 길이를 2의 거듭제곱으로 만들기.cpp
    	내용 : log2, floor, fabs, pow, resize -> pow, ceil, log2

--- Programmers/Programmers/main.cpp
+++ Programmers/Programmers/main.cpp
@@ -1,25 +1,29 @@
-#include <iostream>
-#include <string>
-#include <stack>
+#include <vector>
+#include <cmath>
+
 using namespace std;
 
-int solution(string s)
-{
-    stack<char> stack;
+vector<int> solution(vector<int> arr) {
+    int n = arr.size();
+    if (n == 0) return arr; 
 
-    for (auto c : s)
-    {
-        if (!stack.empty() && c == stack.top()) stack.pop();
-        else  stack.push(c);
+    auto t = log2((double)n);
+
+    auto ft = floor(t);
+    const double eps = 1e-12;
+    if (fabs(t - ft) < eps) {
+        return arr; 
     }
 
-    return stack.empty();
+    int size = pow(2.0, ft + 1.0); 
+    arr.resize(size, 0);                
+    return arr;
 }
 
 int main()
 {
     auto answer = solution(
-        "baabaac"
+        {1,2,3,4,5,6}
     );
     return 0;
 }
